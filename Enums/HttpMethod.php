749ad6ffc9fd70b561d7ad8cd719a3e4e89121c0
Patryk <contact@patryknamyslak.pl>
2026-03-12 16:04:14 +0000

    Create HttpMethod.php

---
+++ Enums/HttpMethod.php
@@ -0,0 +1,15 @@
+<?php
+namespace Probe\Support\Enums;
+
+enum HttpMethod: string{
+    // Fetch a resource
+    case GET = "GET";
+    // Create a new resource
+    case POST = "POST";
+    // A full replacement of a resource
+    case PUT = "PUT";
+    // Partial Update to a resource
+    case PATCH = "PATCH";
+    // Delete a resource
+    case DELETE = "DELETE";
+}
